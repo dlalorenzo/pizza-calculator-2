bc788714fa940f584b7befbd3a3125f8e98ac2a5
lorenzo <99051451@mydavinci.nl>
2020-04-07 14:02:23 +0200

    pizza homepage

---
+++ JavaScript/logic.js
@@ -0,0 +1,12 @@
+const pizzas = [
+{name: "tonno", price: "10"},
+{name: "hawaii", price: "5"},
+{name: "salami", price: "6"}
+];
+
+for(i = 0; i < pizzas.lenght; i++){
+console.log(pizzas[i].name)
+console.log(pizzas[i].price)
+};
+
+//document.getElementById("tonno").innerHTML = "tonijn";
