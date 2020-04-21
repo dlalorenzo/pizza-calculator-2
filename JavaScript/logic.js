9d5a30366ea3a72074a0744ba0454c4996491e92
lorenzo <99051451@mydavinci.nl>
2020-04-21 13:32:19 +0200

    front end bijgewerkt

--- JavaScript/logic.js
+++ JavaScript/logic.js
@@ -1,12 +1,34 @@
 const pizzas = [
-{name: "tonno", price: "10"},
-{name: "hawaii", price: "5"},
-{name: "salami", price: "6"}
+{name: "Tonno", price: "10", toppings: "tomato saus, mozzarella, red unions, tonno"},
+{name: "Hawaii", price: "5", toppings: "tomato saus, mozzarella, ham, pineaple "},
+{name: "Salami", price: "6", toppings: "tomato saus, mozzarella, pepperoni"},
+{name: "Mozzarella", price: "6", toppings: "tomato saus, mozzarella, tomato, pesto"},
+{name: "Chicken-kebab", price: "6", toppings: "tomato saus, mozzarella, red unions, chicken-kebab, garlicsaus"},
+{name: "Prosciutto", price: "6", toppings: "tomato saus, mozzarella, tomato,  prosciutto, oregano, rucola"},
+{name: "Quattro-formaggi", price: "6", toppings: "tomato saus, mozzarella, ricotta, gorgonzola, pecorino"},
+{name: "Margarita", price: "6", toppings: "tomato saus, mozzarella, oregano"},
+{name: "Hot-Spicy", price: "6", toppings: "tomato saus, mozzarella, chicken, red union, paprika, pepperoni, peppers"},
+{name: "funghi", price: "6", toppings: "tomato saus, mozzarella, mushroom, oregano"}
 ];
 
-for(i = 0; i < pizzas.lenght; i++){
+for(i = 0; i < pizzas.length; i++){
 console.log(pizzas[i].name)
 console.log(pizzas[i].price)
+console.log(pizzas[i].toppings)
 };
 
-//document.getElementById("tonno").innerHTML = "tonijn";
+// let x = document.getElementById("tonno");
+// x.innerHTML = "tonijn";
+// console.log(x)
+
+document.getElementById("tonno").innerHTML = "tonijn";
+document.getElementById("hawaii").innerHTML = "hawaii";
+document.getElementById("salami").innerHTML = "pepperoni";
+document.getElementById("mozzarella").innerHTML = "mozzarella";
+document.getElementById("chicken-kebab").innerHTML = "chicken-kebab";
+document.getElementById("prosciutto").innerHTML = "prosciutto";
+document.getElementById("shoarma").innerHTML = "shoarma";
+document.getElementById("quattro-formaggi").innerHTML = "quattro-formaggi";
+document.getElementById("margarita").innerHTML = "margarita";
+document.getElementById("hot-spicy").innerHTML = "hot&spicy";
+document.getElementById("funghi").innerHTML = "funghi";
